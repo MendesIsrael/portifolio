7a168e7f284b670d66b59cdbc8eee322acfeb87e
Israel Mendes da Silva <israel.mendes97@hotmail.com>
2023-10-31 22:45:11 -0300

    atualização

--- my-projects/src/pages/projetos.tsx
+++ my-projects/src/pages/projetos.tsx
@@ -1,10 +1,13 @@
-import { ContainerGlobal } from "@/styles/pages/habilidades";
+import { ContainerGlobal, HabilidadesTitle } from "@/styles/pages/habilidades";
 
 export default function Projetos() {
   return (
     <ContainerGlobal>
-      <h1>PROJETOS</h1>
-      <p>Conheça meus projetos. Esses porjetos são todos pessoas, nenhum deles foram com fins lucrativos ou algo nesse sentido.</p>
+      <HabilidadesTitle>
+        <h1>PROJETOS</h1>
+        <p>Conheça meus projetos. Esses porjetos são todos pessoas, nenhum deles foram com fins lucrativos ou algo nesse sentido.</p>
+      </HabilidadesTitle>
+      
     </ContainerGlobal>
   )
 }
---
+++ my-projects/src/styles/pages/projetos.ts
@@ -0,0 +1,5 @@
+import { styled } from "@stitches/react";
+
+export const Portifolios = styled ('div', {
+
+});
