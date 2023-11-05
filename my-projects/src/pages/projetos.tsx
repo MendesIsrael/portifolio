c8e883f1d388f5aa04548bc01b0b5540203ad7d4
Israel Mendes da Silva <israel.mendes97@hotmail.com>
2023-11-05 00:01:03 -0300

    0

--- my-projects/src/pages/projetos.tsx
+++ my-projects/src/pages/projetos.tsx
@@ -1,4 +1,5 @@
 import { ContainerGlobal, HabilidadesTitle } from "@/styles/pages/habilidades";
+import { Portifolios, Sites, ThreeSites } from "@/styles/pages/projetos";
 
 export default function Projetos() {
   return (
@@ -7,7 +8,24 @@ export default function Projetos() {
         <h1>PROJETOS</h1>
         <p>Conheça meus projetos. Esses porjetos são todos pessoas, nenhum deles foram com fins lucrativos ou algo nesse sentido.</p>
       </HabilidadesTitle>
-      
+      <Portifolios>
+        <ThreeSites>
+          <Sites>
+          </Sites>
+          <Sites>
+          </Sites>
+          <Sites>
+          </Sites>
+        </ThreeSites>
+        <ThreeSites>
+          <Sites>
+          </Sites>
+          <Sites>
+          </Sites>
+          <Sites>
+          </Sites>
+        </ThreeSites>
+      </Portifolios>
     </ContainerGlobal>
   )
 }
--- my-projects/src/styles/pages/projetos.ts
+++ my-projects/src/styles/pages/projetos.ts
@@ -1,5 +1,28 @@
 import { styled } from "@stitches/react";
 
 export const Portifolios = styled ('div', {
+  display               : 'grid',
+  alignItems            : 'center',
+  justifyContent        : 'center',
+  gap                   : '2rem',
+  backgroundColor       : "Aqua",
+});
+
+export const ThreeSites = styled ('div', {
+  display               : 'flex',
+  gap                   : '4rem',
+  backgroundColor       : "red",
+
+});
+
+export const Sites = styled ('div', {
+  height                : '20rem',
+  width                 : '20rem',
+  backgroundColor       : "BlueViolet",
+
+  '&:hover': {
+    height              : '25rem',
+    width               : '25rem',
+  }
 
 });
